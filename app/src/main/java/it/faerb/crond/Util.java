24b715e4668e0930fc47af7b2787f87a5dd77bfe
Lincoln Ramsay <a1291762@gmail.com>
2022-09-22 09:43:43 +1000

    Update for Android 13

--- app/src/main/java/it/faerb/crond/Util.java
+++ app/src/main/java/it/faerb/crond/Util.java
@@ -7,9 +7,8 @@ class Util {
     public static int getColor(Context context, int id) {
         if (Build.VERSION.SDK_INT >= 23) {
             return context.getColor(id);
-        }
-        else {
-            return context.getColor(id);
+        } else {
+            return context.getResources().getColor(id);
         }
     }
 }
