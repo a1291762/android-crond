e8616aa2482e29c4f2b9e037efd518f55ae0a05c
Lincoln Ramsay <a1291762@gmail.com>
2022-11-19 14:26:02 +1000

    Add the foreground service back
    This is an effort to stop alarms from working after around 3 days...
    If this doesn't work, the forcing of background optimization will
    be needed.

--- app/src/main/java/net/yasmar/crond/AlarmReceiver.java
+++ app/src/main/java/net/yasmar/crond/AlarmReceiver.java
@@ -3,48 +3,20 @@ package net.yasmar.crond;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.PowerManager;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import eu.chainfire.libsuperuser.Shell;
-
-import static net.yasmar.crond.Constants.INTENT_EXTRA_LINE_NAME;
-import static net.yasmar.crond.Constants.INTENT_EXTRA_LINE_NO_NAME;
-import static net.yasmar.crond.Constants.PREFERENCES_FILE;
-import static net.yasmar.crond.Constants.PREF_USE_WAKE_LOCK;
+import android.util.Log;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
     private static final String TAG = "AlarmReceiver";
 
-    ExecutorService executor = Executors.newSingleThreadExecutor();
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        executor.execute(() -> {
-            SharedPreferences sharedPrefs = context.getSharedPreferences(PREFERENCES_FILE,
-                    Context.MODE_PRIVATE);
-            final PowerManager.WakeLock wakeLock;
-            if (sharedPrefs.getBoolean(PREF_USE_WAKE_LOCK, false)) {
-                PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-                wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG+":wakeLock");
-                wakeLock.acquire();
-            } else {
-                wakeLock = null;
-            }
-            Crond crond = new Crond(context);
-            String line = intent.getExtras().getString(INTENT_EXTRA_LINE_NAME);
-            int lineNo = intent.getExtras().getInt(INTENT_EXTRA_LINE_NO_NAME);
-            IO.rootAvailable = Shell.SU.available();
-            IO.nonRootPrefix = context.getExternalFilesDir(null);
-            crond.executeLine(line, lineNo);
-            crond.scheduleLine(line, lineNo, false, false, false);
-            if (wakeLock != null) {
-                wakeLock.release();
-            }
-        });
+        Log.i(TAG, "AlarmReceiver START onReceive");
+        Intent serviceIntent = new Intent(context, JobRunnerService.class);
+        serviceIntent.setAction("start");
+        serviceIntent.putExtras(intent);
+        context.startForegroundService(serviceIntent);
+        Log.i(TAG, "AlarmReceiver STOP onReceive");
     }
+
 }
